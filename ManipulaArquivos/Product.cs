7b899eaa8aee11398c23eacd986e401629037dba
veronezzi159 <lucasveronezzi3@gmail.com>
2024-05-07 15:45:09 -0300

    commit 1545

---
+++ ManipulaArquivos/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulaArquivos
+{
+    internal class Product
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; }
+
+        public Product(int id, string d, double p, int qtd)
+        {
+            this.id = id;
+            this.description = d;
+            this.price = p;
+            this.quantity = qtd;
+        }
+    }
+}
--- ManipulaArquivos/Program.cs
+++ ManipulaArquivos/Program.cs
@@ -1,65 +1,85 @@
-﻿string path;
-string file = "arquivo.txt";
+﻿using ManipulaArquivos;
 
-path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Dados\\";
-//path = @"C:\Dados\";
+string path = @"C:\DadosEstoque\";
+string file = "products.txt";
 
-if (!Directory.Exists(path))
-    Directory.CreateDirectory(path);
-if (File.Exists(path + file))
+Product CreateProduct()
 {
-    StreamReader sr = new StreamReader(path + file);    
-    Console.WriteLine("Digite um nome: ");
-    string s = sr.ReadToEnd();
-    s += "\n";
-    s += Console.ReadLine();
-    sr.Close();
-    StreamWriter sw = new StreamWriter(path + file);
-    sw.WriteLine(s);
-    sw.Close();
+    Console.WriteLine("Informe um Id");
+    var id = int.Parse(Console.ReadLine());
 
-    sr = new StreamReader(path + file);
-    Console.WriteLine(sr.ReadToEnd());
+    Console.WriteLine("Informe a descrção do produto");
+    string description = Console.ReadLine();
 
-    string retorno = File.ReadLines(path + file).ElementAt(4);
-    Console.WriteLine(retorno);
-}
-else
-{
-    File.Create(path + file);
-    StreamWriter sw = new StreamWriter(path + file);
-    Console.WriteLine("Digite: ");
-    sw.WriteLine(Console.ReadLine());
-    sw.Close();
+    Console.WriteLine("Informe o preço");
+    double price = double.Parse(Console.ReadLine());
 
-    StreamReader sr = new StreamReader(path + file);
-    string s = sr.ReadToEnd();
-    sr.Close();
+    Console.WriteLine("Informe a quantidade disponivel: ");
+    int qtd = int.Parse(Console.ReadLine());
 
-    sw = new StreamWriter(path + file);
-
-    sw.Write(s+ "\n" + Console.ReadLine());
-
+    return new(id,description,price,qtd);
 }
 
+void ShowAll(List <Product> l)
+{
+    foreach (Product p in l)
+    {
+        Console.WriteLine(p.ToString());
+    }
+}
+
+bool CheckIfExist(string p, string f)
+{
+    if (!Directory.Exists(p))
+        Directory.CreateDirectory(p);
+
+    if (!File.Exists(p + f))
+        File.Create(p + f);
+    return true;
+}
+
+void SaveFile(List <Product> list, string ph, string f)
+{
+    if (CheckIfExist(ph, f))
+    {
+        StreamWriter streamWriter = new StreamWriter(ph + f);
+
+        foreach (Product p in list)
+        {
+            streamWriter.WriteLine(p.ToString());
+        }
+        streamWriter.Close();
+    }    
+}
+
+List <Product> LoadFile(string ph, string f)
+{
+    List <Product> l = new List <Product>();
+
+    if (CheckIfExist(ph,f))
+    {
+        StreamReader reader = new StreamReader(ph + f);
+        string[] data = new string[4];
+        foreach (var linha in File.ReadAllLines(ph + f))
+        {
+            data = linha.Split(";");
+            l.Add(new Product(int.Parse(data[0]), data[1], double.Parse(data[2]), int.Parse(data[3])));
+        }
+    }
+    return l;
+}
 
 
-/*StreamReader sr = new(path + file);
-StreamWriter sw = new(path + file);
+List <Product> products = new List <Product>();
 
-Console.WriteLine("Informe seu nome");
-string s = Console.ReadLine();
+Console.WriteLine("Cadastro de produtos");
 
+products.Add(CreateProduct());
 
-sw.WriteLine(s); // escreve linha no arquivo
+ShowAll(products);
 
+products.Add(CreateProduct());
 
-Console.WriteLine("Informe seu email: ");
-s = Console.ReadLine();
+ShowAll(products);
 
-sw.WriteLine(s); // escreve linha no arquivo
-sw.Close(); // fecha arquivo
-
-Console.Clear();
-Console.WriteLine(sr.ReadToEnd());
-sr.Close();*/
+SaveFile(products, path, file);
